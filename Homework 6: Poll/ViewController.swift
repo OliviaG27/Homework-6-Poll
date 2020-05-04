95d5f14da38894003836b805ec5812e7a7fa3f92
Olivia Gennaro <o.gennaro@setonhill.edu>
2020-05-04 03:09:14 -0400

    Initial Commit

---
+++ Homework 6: Poll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 6: Poll
+//
+//  Created by Olivia Gennaro on 5/4/20.
+//  Copyright © 2020 Olivia Gennaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
